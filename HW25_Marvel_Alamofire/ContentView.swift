8d72b37143017e5496218bf350d9ae00c601dc1d
a.kravcov <a.kravcov@movavi.com>
2022-06-24 14:28:53 +0700

    Initial Commit

---
+++ HW25_Marvel_Alamofire/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW25_Marvel_Alamofire
+//
+//  Created by a.kravcov on 24.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW25_Marvel_Alamofire/HW25_Marvel_AlamofireApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW25_Marvel_AlamofireApp.swift
+//  HW25_Marvel_Alamofire
+//
+//  Created by a.kravcov on 24.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW25_Marvel_AlamofireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
